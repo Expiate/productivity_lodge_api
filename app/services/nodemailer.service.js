1991ca65222b568c3eaa3004cee0f9000d1a0f5e
Expiate <Tuetano32@gmail.com>
2021-04-17 17:47:29 +0200

    Added logic to nodemailer.service.js

--- app/services/nodemailer.service.js
+++ app/services/nodemailer.service.js
@@ -1,3 +1,27 @@
 const { config } = require("dotenv/types");
 
 config()
+
+const nodemailer = require("nodemailer");
+
+const transport = nodemailer.createTransport({
+    service: "Gmail",
+    auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS,
+    },
+});
+
+module.exports.sendConfirmationEmail = (name, email, confirmationCode) => {
+    console.log("Check");
+    transport.sendMail({
+        from: user,
+        to: email,
+        subject: "Please confirm your account",
+        html: `<h1>Email Confirmation</h1>
+            <h2>Hello ${name}</h2>
+            <p>Thank you for using Productivity Lodge. Please confirm your email by clicking on the following link</p>
+            <a href=http://localhost:3000/confirm/${confirmationCode}>Click here</a>
+            </div>`,
+    }).catch(err => console.log(err));
+};
