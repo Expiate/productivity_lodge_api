5a4243be3e127d8ac04c58deca2b7d4a61bcd72c
Expiate <Tuetano32@gmail.com>
2021-04-17 17:29:48 +0200

    init

---
+++ app/user/user.model.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ['Pending', 'Active'],
+        default: 'Pending'
+    },
+    confirmationCode: {
+        type: String,
+        unique: true
+    }
+    
+})
+
+module.exports = mongoose.model('User', userSchema)
