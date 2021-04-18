e1d99bc1c2b76065c9f442da28d7743d7874df15
Expiate <Tuetano32@gmail.com>
2021-04-18 22:17:12 +0200

    Added doc

--- src/app/middleware/auth.middleware.js
+++ src/app/middleware/auth.middleware.js
@@ -1,5 +1,14 @@
 const jwt = require('jsonwebtoken')
 
+/**
+ * This Function acts a a Middleware to check that a certain request haves a jwt token in its authorization header
+ * and then tries to verify that the token is valid, if its valid it passes its contents to the next Function
+ * 
+ * @param {*} req 
+ * @param {*} res 
+ * @param {*} next 
+ * @returns 
+ */
 module.exports.authenticateToken = (req, res, next) => {
     const authHeader = req.headers['authorization']
     const token = authHeader && authHeader.split(' ')[1]
