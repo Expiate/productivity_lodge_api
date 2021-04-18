b8d03f9a4dcfcccd3c613e39f221cd39a1cba9aa
Expiate <Tuetano32@gmail.com>
2021-04-18 14:51:08 +0200

    Updated logic so the console log appears more clearly

--- src/app/middleware/log.middleware.js
+++ src/app/middleware/log.middleware.js
@@ -6,9 +6,11 @@
  */
 module.exports.log = (app) => {
     app.use((req, res, next) => {
+        console.log('=== API CALL ===')
         console.log(`method: ${req.method}`)
         console.log(`path: ${req.path}`)
         console.log(`body: ${JSON.stringify(req.body)}`)
+        console.log('----------------')
         next();
     })
 }
