3529faf020ad5a49eb3692300065228944033d14
Expiate <Tuetano32@gmail.com>
2021-04-18 20:16:41 +0200

    Added Router Configuration to the server (login)

---
+++ src/app/user/login/login.routes.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const logController = require('./login.controller')
+const router = express.Router()
+
+// Login Route
+router.route('/').post(logController.login)
+
+module.exports = router
--- src/app/user/user.routes.js
+++ src/app/user/user.routes.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const signRouter = require('./signup/signup.routes')
+const logRouter = require('./login/login.routes')
 const userController = require('./user.controller')
 const router = express.Router()
 
 // Nested Router to Signup
 router.use('/signup', signRouter)
+
+// Nested Router to Login
+router.use('/login', logRouter)
 
 // Get All Users Route
 router.route('/').get(userController.getAll)
