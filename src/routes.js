52be56cf5ebc02246b96bd2df165aec6a25a8c6c
Expiate <tuetano32@gmail.com>
2021-05-14 10:02:50 +0200

    Updated global api routes to add Journal Router

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 const userRouter = require('./app/user/user.routes')
 const daysRouter = require('./app/day/day.routes')
+const journalRouter = require('./app/journal/journal.routes')
 
 /**
  * This Function loads all existing Root Routes in the API
@@ -9,4 +10,5 @@ const daysRouter = require('./app/day/day.routes')
 module.exports.loadRoutes = (app) => {
     app.use('/users', userRouter)
     app.use('/days', daysRouter)
+    app.use('/journals', journalRouter)
 }
